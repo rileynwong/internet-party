3cbcd0d92f79a4d32d8c023a518d506008925cc5
nancywong <nancypwong@gmail.com>
2015-08-13 18:57:46 -0400

    Add port as variable

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# internet-party
-hi sophie
+# internet party: 3d picture gallery via MMS
+
+## setup
+
+## usage
+text (706) 956-5023 with a picture message to upload an image to the 3d gallery
+
+
--- app/run.py
+++ app/run.py
@@ -48,8 +48,9 @@ def run():
   return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
-    #app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 isfile = os.path.isfile
 join = os.path.join
